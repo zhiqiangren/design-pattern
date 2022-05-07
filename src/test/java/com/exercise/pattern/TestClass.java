70eac6f605363d16125d5bd61a0b631d7a55abad
nnfan <ningning.fan@163.com>
2022-05-07 23:14:50 +0800

    add test

---
+++ src/test/java/com/exercise/pattern/TestClass.java
@@ -0,0 +1,21 @@
+package com.exercise.pattern;
+
+import org.junit.Test;
+/**
+ *
+ *
+ * @author nnfan
+ * @date 2022/5/7 11:07 下午
+ */
+public class TestClass {
+
+    @Test
+    public void test() {
+
+    }
+
+    @org.junit.Test
+    public void t() {
+
+    }
+}
